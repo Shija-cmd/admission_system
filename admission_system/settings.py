cac26f485ace7d24349bb2f583ba9f411030de80
Shija-cmd <shijajuma@protonmail.com>
2023-10-18 00:24:27 +0300

    Another commit

--- admission_system/settings.py
+++ admission_system/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2irzxa=si*13%u1#xt#t9a1dh42uvlq$y_yw)+v0or*99an^9!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
